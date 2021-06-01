8fc79f88e35ba6177a27ef53a3d143ad8e3dc745
Anders Kok <ak.kornerup@gmail.com>
2021-06-01 11:12:25 +0200

    Wrote shit

--- Fysik-Brydning/public/dist/main.js
+++ Fysik-Brydning/public/dist/main.js
@@ -91,26 +91,56 @@
 "use strict";
 
 /// <reference path="../node_modules/@types/p5/global.d.ts"/>
-var cnvWidth = 600;
-var cnvHeight = 400;
-var cam;
-var angle = 0;
-var angleOffset = 0;
+var waterSurface = 0;
+var uppperBrydning = 1;
+var lowerBrydning = 1.33;
+var criticalVinkel = 0;
 window.setup = function () {
-    createCanvas(cnvWidth, cnvHeight);
-    angleOffset = random(-0.1, 0.1);
+    createCanvas(windowWidth, windowHeight);
+    waterSurface = height / 2;
+    angleMode(DEGREES);
+    criticalVinkel = asin(uppperBrydning / lowerBrydning);
 };
 window.draw = function () {
-    colorMode(HSB, 255);
-    background(frameCount % 255, 255, 255);
-    translate(cnvWidth / 2, cnvHeight / 2);
-    rectMode(CENTER);
+    background(255);
+    // Draw water surface line
+    strokeWeight(1);
+    stroke(0);
+    line(0, waterSurface, width, waterSurface);
+    // Draw water color
     noStroke();
-    if (frameCount % 30 == 0)
-        angleOffset = random(-0.1, 0.1);
-    angle += angleOffset;
-    rotate(angle);
-    rect(0, 0, 50, 50);
+    fill(50, 50, 255, 50);
+    rect(0, waterSurface, width, height);
+    // Calculate laser
+    var straightVector = createVector(1, 0);
+    var mouseVector = createVector(mouseX, mouseY);
+    var angle = straightVector.angleBetween(mouseVector);
+    var laserLength = waterSurface / sin(angle);
+    var laserX = cos(angle) * laserLength;
+    var laserY = sin(angle) * laserLength;
+    var indfaldsVinkel = 180 - 90 - angle;
+    var waterIntersectX = waterSurface / tan(angle);
+    // Draw laser
+    strokeWeight(3);
+    stroke(255, 100, 100);
+    line(0, 0, laserX, laserY);
+    // Calculate new laser path
+    var brydningVinkel = 90 - asin((sin(indfaldsVinkel) * uppperBrydning) / lowerBrydning);
+    var newAngle = indfaldsVinkel > criticalVinkel ? -90 + indfaldsVinkel : brydningVinkel;
+    var newLaserX = cos(newAngle) * width + waterIntersectX;
+    var newLaserY = sin(newAngle) * width + waterSurface;
+    // Draw new laser path
+    strokeWeight(3);
+    stroke(255, 100, 100);
+    line(waterIntersectX, waterSurface, newLaserX, newLaserY);
+    // Random debug
+    noStroke();
+    fill(0);
+    strokeWeight(0);
+    text("indfaldsVinkel: " + indfaldsVinkel.toFixed(2), 10, 200);
+    text("criticalVinkel: " + criticalVinkel.toFixed(2), 10, 210);
+    text("brydningVinkel: " + brydningVinkel.toFixed(2), 10, 220);
+    text("angle: " + angle.toFixed(2), 10, 230);
 };
 
 
--- Fysik-Brydning/src/main.ts
+++ Fysik-Brydning/src/main.ts
@@ -1,25 +1,63 @@
 /// <reference path="../node_modules/@types/p5/global.d.ts"/>
 
-const cnvWidth = 600;
-const cnvHeight = 400;
-
-let angle = 0;
-let angleOffset = 0;
+let waterSurface: number = 0;
+const uppperBrydning = 1;
+const lowerBrydning = 1.33;
+let criticalVinkel = 0;
 
 (window as any).setup = () => {
-	createCanvas(600, 400);
-	angleOffset = random(-0.1, 0.1);
+	createCanvas(windowWidth, windowHeight);
+    waterSurface = height / 2;
+    angleMode(DEGREES);
+    criticalVinkel = asin(uppperBrydning / lowerBrydning);
 }
 
 (window as any).draw = () => {
-	colorMode(HSB, 255);
-	background(frameCount % 255, 255, 255);
-	translate(width / 2, height / 2);
-	rectMode(CENTER);
-	noStroke();
+    background(255);
 
-	if(frameCount % 30 == 0) angleOffset = random(-0.1, 0.1);
-	angle += angleOffset;
-	rotate(angle);
-	rect(0, 0, 50, 50);
+    // Draw water surface line
+    strokeWeight(1);
+    stroke(0);
+    line(0, waterSurface, width, waterSurface);
+
+    // Draw water color
+    noStroke();
+    fill(50, 50, 255, 50);
+    rect(0, waterSurface, width, height);
+
+    // Calculate laser
+    const straightVector = createVector(1, 0);
+    const mouseVector = createVector(mouseX, mouseY);
+    const angle = straightVector.angleBetween(mouseVector);
+    const laserLength = waterSurface / sin(angle);
+    const laserX = cos(angle) * laserLength;
+    const laserY = sin(angle) * laserLength;
+    const indfaldsVinkel = 180 - 90 - angle;
+    const waterIntersectX = waterSurface / tan(angle);
+
+    // Draw laser
+    strokeWeight(3);
+    stroke(255, 100, 100);
+    line(0, 0, laserX, laserY);
+
+    // Calculate new laser path
+    const brydningVinkel = 90 - asin( ( sin(indfaldsVinkel) * uppperBrydning ) / lowerBrydning );
+    const newAngle = indfaldsVinkel > criticalVinkel ? -90 + indfaldsVinkel : brydningVinkel;
+    
+    const newLaserX = cos(newAngle) * width + waterIntersectX;
+    const newLaserY = sin(newAngle) * width + waterSurface;
+
+    // Draw new laser path
+    strokeWeight(3);
+    stroke(255, 100, 100);
+    line(waterIntersectX, waterSurface, newLaserX, newLaserY);
+
+    // Random debug
+    noStroke();
+    fill(0);
+    strokeWeight(0);
+    text("indfaldsVinkel: " + indfaldsVinkel.toFixed(2), 10, 200);
+    text("criticalVinkel: " + criticalVinkel.toFixed(2), 10, 210);
+    text("brydningVinkel: " + brydningVinkel.toFixed(2), 10, 220);
+    text("angle: " + angle.toFixed(2), 10, 230);
 }
