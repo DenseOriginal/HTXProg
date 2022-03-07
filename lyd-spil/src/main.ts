217bd7a2c9539dac1d7431dc8940c1a4d2ef60bf
Anders Kok <48748922+DenseOriginal@users.noreply.github.com>
2022-03-07 13:54:07 +0100

    Initialized new project

---
+++ lyd-spil/src/main.ts
@@ -0,0 +1,22 @@
+/// <reference path="../node_modules/@types/p5/global.d.ts"/>
+
+let angle = 0;
+let angleOffset = 0;
+
+(window as any).setup = () => {
+	createCanvas(windowWidth, windowHeight);
+	angleOffset = random(-0.1, 0.1);
+}
+
+(window as any).draw = () => {
+	colorMode(HSB, 255);
+	background(frameCount % 255, 255, 255);
+	translate(width / 2, height / 2);
+	rectMode(CENTER);
+	noStroke();
+
+	if(frameCount % 30 == 0) angleOffset = random(-0.1, 0.1);
+	angle += angleOffset;
+	rotate(angle);
+	rect(0, 0, 50, 50);
+}
