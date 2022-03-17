c673113dfeed067f5c7ab82a6074a30b0b296131
Anders Kok <48748922+DenseOriginal@users.noreply.github.com>
2022-03-17 13:49:41 +0100

    New and improved

--- lyd-drumpad/app.js
+++ lyd-drumpad/app.js
@@ -1,26 +1,24 @@
 const sounds = {
-  1: "./sounds/fuck-din-far.mp3",
-  2: "./sounds/fuck-din-mor.mp3",
-  3: "./sounds/boom.mp3",
-  4: "./sounds/meow.mp3",
-  5: "./sounds/pornhub.mp3",
-  6: "./sounds/minecraft-villager.mp3",
-  7: "./sounds/bababooey.mp3",
-  8: "./sounds/bruh.mp3",
-  9: "./sounds/sans.mp3",
+  1: audioFromLink("./sounds/fuck-din-far.mp3"),
+  2: audioFromLink("./sounds/fuck-din-mor.mp3"),
+  " ": audioFromLink("./sounds/boom.mp3"),
+  4: audioFromLink("./sounds/meow.mp3"),
+  // 5: audioFromLink("./sounds/pornhub.mp3"),
+  6: audioFromLink("./sounds/minecraft-villager.mp3"),
+  7: audioFromLink("./sounds/bababooey.mp3"),
+  "ArrowRight": audioFromLink("./sounds/bruh.mp3"),
+  9: audioFromLink("./sounds/sans.mp3"),
+  "ArrowLeft": audioFromOscillator(440, 'sine'),
 };
+
+const audioSources = {};
 
 // Loop over all the sound and create an audio element for that sound, then add it to the DOM
 for (let key in sounds) {
-  let audio = document.createElement("audio");
-  audio.setAttribute("id", key);
-  audio.setAttribute("src", sounds[key]);
-  document.body.appendChild(audio);
-
   // Create a button with the sound name and add it to the .container div
   let button = document.createElement("button");
   button.dataset.soundkey = key;
-  button.innerHTML = sounds[key].slice(9, -4).replaceAll('-', ' ');
+  button.innerHTML = sounds[key].name;
   document.getElementById('container').appendChild(button);
 
   // Give all the buttons a random pastel color
@@ -30,6 +28,7 @@ for (let key in sounds) {
 
 // Listen for keyboard events, and play the sound when the key is pressed, using the playSound function
 document.addEventListener("keydown", function(event) {
+  console.log(event.key);
   playSound(event.key);
   const button = document.querySelector(`button[data-soundkey="${event.key}"]`);
   if(!button) return;
@@ -38,6 +37,7 @@ document.addEventListener("keydown", function(event) {
 
 // Remove active class from all buttons when a key is released
 document.addEventListener("keyup", function(event) {
+  stopSound(event.key);
   const button = document.querySelector(`button[data-soundkey="${event.key}"]`);
   if(!button) return;
   button.classList.remove('active');
@@ -53,8 +53,36 @@ document.querySelectorAll('button').forEach(function(button) {
 
 // Function to take in a key and play the sound
 function playSound(key) {
-  let audio = document.getElementById(key);
+  let audio = sounds[key];
   if(!audio) return;
-  audio.currentTime = 0;
-  audio.play();
+  audio.start();
+}
+
+// Function to take in a key and stop the sound
+function stopSound(key) {
+  let audio = sounds[key];
+  if(!audio) return;
+  audio.stop();
+}
+
+function audioFromLink(link) {
+  let audio = document.createElement("audio");
+  audio.setAttribute("src", link);
+  document.body.appendChild(audio);
+
+  return {
+    start: () => { audio.currentTime = 0; audio.play(); },
+    stop: () => {},
+    name: link.slice(9, -4).replaceAll('-', ' '),
+  }
+}
+
+function audioFromOscillator(freq, type) {
+  const osc = new p5.Oscillator(freq, type);
+
+  return {
+    start: () => osc.start(),
+    stop: () => osc.stop(),
+    name: `${type} ${freq} Hz`,
+  };
 }
