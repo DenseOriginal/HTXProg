7f3462f50db580f8fd448709546e69b416e204e9
Anders Kok <48748922+DenseOriginal@users.noreply.github.com>
2022-03-21 22:03:18 +0100

    Complete overhaul

--- lyd-drumpad/app.js
+++ lyd-drumpad/app.js
@@ -1,88 +1,10 @@
-const sounds = {
-  1: audioFromLink("./sounds/fuck-din-far.mp3"),
-  2: audioFromLink("./sounds/fuck-din-mor.mp3"),
-  " ": audioFromLink("./sounds/boom.mp3"),
-  4: audioFromLink("./sounds/meow.mp3"),
-  // 5: audioFromLink("./sounds/pornhub.mp3"),
-  6: audioFromLink("./sounds/minecraft-villager.mp3"),
-  7: audioFromLink("./sounds/bababooey.mp3"),
-  "ArrowRight": audioFromLink("./sounds/bruh.mp3"),
-  9: audioFromLink("./sounds/sans.mp3"),
-  "ArrowLeft": audioFromOscillator(440, 'sine'),
-};
-
-const audioSources = {};
-
-// Loop over all the sound and create an audio element for that sound, then add it to the DOM
-for (let key in sounds) {
-  // Create a button with the sound name and add it to the .container div
-  let button = document.createElement("button");
-  button.dataset.soundkey = key;
-  button.innerHTML = sounds[key].name;
-  document.getElementById('container').appendChild(button);
-
-  // Give all the buttons a random pastel color
-  button.style.setProperty('--color', `hsl(${Math.floor(Math.random() * 360)}, 100%, 90%)`);
-}
-
-
-// Listen for keyboard events, and play the sound when the key is pressed, using the playSound function
-document.addEventListener("keydown", function(event) {
-  console.log(event.key);
-  playSound(event.key);
-  const button = document.querySelector(`button[data-soundkey="${event.key}"]`);
-  if(!button) return;
-  button.classList.add('active');
-});
-
-// Remove active class from all buttons when a key is released
-document.addEventListener("keyup", function(event) {
-  stopSound(event.key);
-  const button = document.querySelector(`button[data-soundkey="${event.key}"]`);
-  if(!button) return;
-  button.classList.remove('active');
-});
-
-// Loop over all buttons and add and event listener to each one
-// When a button is clicked, play the sound associated with the button
-document.querySelectorAll('button').forEach(function(button) {
-  button.addEventListener('mousedown', function() {
-    playSound(button.dataset.soundkey);
-  });
-});
-
-// Function to take in a key and play the sound
-function playSound(key) {
-  let audio = sounds[key];
-  if(!audio) return;
-  audio.start();
-}
-
-// Function to take in a key and stop the sound
-function stopSound(key) {
-  let audio = sounds[key];
-  if(!audio) return;
-  audio.stop();
-}
-
-function audioFromLink(link) {
-  let audio = document.createElement("audio");
-  audio.setAttribute("src", link);
-  document.body.appendChild(audio);
-
-  return {
-    start: () => { audio.currentTime = 0; audio.play(); },
-    stop: () => {},
-    name: link.slice(9, -4).replaceAll('-', ' '),
-  }
-}
-
-function audioFromOscillator(freq, type) {
-  const osc = new p5.Oscillator(freq, type);
-
-  return {
-    start: () => osc.start(),
-    stop: () => osc.stop(),
-    name: `${type} ${freq} Hz`,
-  };
-}
+new Pad('Fuck din far', new AudioFromLink("./sounds/fuck-din-far.mp3"), 1);
+new Pad('Fuck din mor', new AudioFromLink("./sounds/fuck-din-mor.mp3"), 2);
+new Pad('Boom', new AudioFromLink("./sounds/boom.mp3"), 3);
+new Pad('Cat', new AudioFromLink("./sounds/meow.mp3"), 4);
+new Pad('Pornhub', new AudioFromLink("./sounds/pornhub.mp3"), 5);
+new Pad('Minecraft villager', new AudioFromLink("./sounds/minecraft-villager.mp3"), 6);
+new Pad('Bababooey', new AudioFromLink("./sounds/bababooey.mp3"), 7);
+new Pad('Bruh', new AudioFromLink("./sounds/bruh.mp3"), 9);
+new Pad('Sans', new AudioFromLink("./sounds/sans.mp3"), 0);
+new Pad('Sine 440 hz', new AudioFromOscillator(440, 'sine'), 'a');
---
+++ lyd-drumpad/classes.js
@@ -0,0 +1,48 @@
+class AudioFromLink {
+  audio;
+
+  constructor(link) {
+    this.audio = new Audio(link);
+  }
+
+  start() { this.audio.currentTime = 0; this.audio.play() }
+  stop() {  }
+}
+
+class AudioFromOscillator {
+  osc;
+
+  constructor(freq, type) {
+    this.osc = new p5.Oscillator(freq, type);
+  }
+
+  start() { this.osc.start() }
+  stop() { this.osc.stop() }
+}
+
+class Pad {
+  audioSource;
+  name;
+  key;
+  htmlButton;
+
+  constructor(name, audioSource, key) {
+    this.name = name;
+    this.audioSource = audioSource;
+    this.key = key;
+    
+    let button = document.createElement("button");
+    button.innerHTML = name;
+    document.getElementById('container').appendChild(button);
+
+    this.htmlButton = button;
+
+    // Setup button listeners
+    button.addEventListener('mousedown', () => this.audioSource.start());
+    button.addEventListener('mouseup', () => this.audioSource.stop());
+
+    // Setup key listeners
+    document.addEventListener("keydown", (e) => { if(e.key == key) { this.audioSource.start(); button.classList.add('active') } });
+    document.addEventListener("keyup", (e) => { if(e.key == key) { this.audioSource.stop(); button.classList.remove('active') } });
+  }
+}
