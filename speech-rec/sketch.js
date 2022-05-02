08a8812b4d7509b4caf4ec2aa032268b428670aa
Anders Kok <48748922+DenseOriginal@users.noreply.github.com>
2022-05-02 21:11:31 +0200

    Commit

--- speech-rec/sketch.js
+++ speech-rec/sketch.js
@@ -17,7 +17,7 @@ const myRec = new p5.SpeechRec('en-US', () => {
 myRec.continuous = true; // do continuous recognition
 myRec.interimResults = true; // allow partial recognition (faster, less accurate)
 
-const triggerWord = "trigger";
+const triggerWord = "record";
 
 function setup() {
   myRec.start();
