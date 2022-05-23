b784dd1e2038d7ce6d7d0fd710c9cacbeb7934dd
Anders Kok <48748922+DenseOriginal@users.noreply.github.com>
2022-05-23 22:07:45 +0200

    Fixed angle

--- Funktion-spil/src/paths/generic.ts
+++ Funktion-spil/src/paths/generic.ts
@@ -2,6 +2,6 @@ export abstract class GenericPath {
   abstract calculate(x: number): number;
 
   getAngle(x: number): number {
-    return atan( 2 / (this.calculate(x-1) - this.calculate(x+1)) ) + 90;
+    return atan( (this.calculate(x-1) - this.calculate(x+1)) / 2 );
   }
 }
